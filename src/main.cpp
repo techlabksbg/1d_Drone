13a53b948e9786f6649d0b7689b6167afa73a66d
PhilKu7 <ghphil@zab.li>
2021-09-24 20:19:43 +0200

    control working with Kp

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,14 @@
 #include <Wire.h>
 #include <Servo.h>
 
-const int MPU_addr = 0x68;  // I2C address of the MPU-6050
-const int avg_number = 220; //100; //oben = 0; unten ca. 300
+const int MPU_addr = 0x68; // I2C address of the MPU-6050
+const int avg_number = 10;
 int16_t average[avg_number];
-int16_t index = 0, setPoint=230, setPointBegin = 0;
+int16_t index = 0, setPoint = 0, setPointBegin = 0;
 float sum = 0;
-float AcX, error;
-float turn, Kp = 0.007, Ki = 0.00001, Kd = 0.0001, derivative = 0, lastError = 0, integral;
-Servo drone;
+float AcY, error; // AcY maximum (top) = 2500, minimum (bottom) = -3300, 0 = best
+float turn, Kp = 0.001, Ki = 0, Kd = 0, derivative = 0, lastError = 0, integral;
+Servo drone; // drone.write() //50 = minimum, 65 = maximum
 void setup()
 {
     Wire.begin();
@@ -22,22 +22,6 @@ void setup()
     drone.write(20);
     delay(2000);
     drone.write(60);
-
-//    Wire.beginTransmission(MPU_addr);
-//    Wire.write(0x3B); // starting with register 0x3B (ACCEL_XOUT_H)
-//    Wire.endTransmission(false);
-//    Wire.requestFrom(MPU_addr, 2, true);             // request a total of 2 registers
-//    setPoint = (Wire.read() << 8 | Wire.read())-setPointBegin; // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
-
-//    Wire.beginTransmission(MPU_addr);
-//    Wire.write(0x3B); // starting with register 0x3B (ACCEL_XOUT_H)
-//    Wire.endTransmission(false);
-//    Wire.requestFrom(MPU_addr, 2, true);             // request a total of 2 registers
-//    setPoint = setPointBegin+(Wire.read() << 8 | Wire.read()); // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
-//    Serial.print(setPoint);
-    //  drone.write(25);
-    //  delay(2000);
-    //  drone.write();
 }
 void loop()
 {
@@ -51,19 +35,18 @@ void loop()
         index = 0;
     }
     Wire.beginTransmission(MPU_addr);
-    Wire.write(0x3B); // starting with register 0x3B (ACCEL_XOUT_H)
-    // Wire.write(0x43);  // starting with register 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+    Wire.write(0x3D); // starting with register 0x3D (ACCEL_YOUT_H)
     Wire.endTransmission(false);
     Wire.requestFrom(MPU_addr, 2, true);             // request a total of 2 registers
-    average[index] = Wire.read() << 8 | Wire.read(); // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
+    average[index] = Wire.read() << 8 | Wire.read(); // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
     for (int n = 0; n < avg_number; n++)
     {
         sum += average[n];
     }
-    AcX = sum / avg_number;
-    //drone.write()// 50 = minimum, 65 = maximum
-    error = abs(AcX) - setPoint;
-    Serial.print(error); // 0 = minimum, 320 = maximum
+    AcY = sum / avg_number;
+    error = setPoint - AcY;
+    Serial.print(error);
+    // Serial.print(average[index]);
     Serial.print("\n");
     integral += error;
     derivative = error - lastError;
@@ -71,9 +54,4 @@ void loop()
     drone.write(turn + 60);
     lastError = error;
     delay(1);
-
-    //  drone.write(50);
-    //  delay(1000);
-    //  drone.write(65);
-    //  delay(1000);
 }
